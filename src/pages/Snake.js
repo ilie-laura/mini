488002e932297ebf3f598cd5c9c3c50560601e89
laura <ilie_ioana_laura@yahoo.com>
2025-11-16 19:42:43 +0200

    to fix css

--- src/pages/Snake.js
+++ src/pages/Snake.js
@@ -4,9 +4,7 @@ import SnakeS from "../assets/scripts/Snake.js"
 function Snake() {
   return (
     <div>
-      <h1>Snake Game</h1>
-      <p>Use the arrow keys to move the snake and eat the food!</p>
-      
+
         <SnakeS />
    
     </div>
--- src/pages/XandO.js
+++ src/pages/XandO.js
@@ -2,8 +2,8 @@ import React from 'react'
 import XandO from '../assets/scripts/XandO';
 function XAndO() {
   return (
-    <div>
-      <h1>Buna</h1>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+
       <XandO />
     </div>
   )
